a37917b9837be4b6946f71793d75c6d5a440007f
Abdul Samad <samadjs100@gmail.com>
2025-06-09 00:01:06 +0500

    new 2

--- input.c
+++ input.c
@@ -6,29 +6,22 @@
 
 /*
  This is a
- multi-line comment
 */
-
 int main() {
     int x = 10;
     int y = 20;
     int unusedVar = 50;
-
     if (x == true) {
         printf("x is true\n");
     }
-
     if (y == false) {
         printf("y is false\n");
     }
-
     while (x == true) {
         printf("Still true\n");
         break;
     }
-
     int result = x + y;
-    printf("Result: %d\n", result);
-
+    printf("Result: %d\n", result);\
     return 0;
 }
